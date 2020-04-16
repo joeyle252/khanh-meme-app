cd28dbebfaa71b9ea698e70a278cc25e7f7e8846
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-04-16 16:26:47 +0700

    feat: working in process

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var Jimp = require('jimp');
 const upload = require("../utils/upload");
-const {loadData, saveData} = require("../utils/data.js");
+const { loadData, saveData } = require("../utils/data.js");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: "Khanh's Meme app" });
 });
 
-router.get("/browser",(req,res)=>{
+router.get("/browser", (req, res) => {
   const data = loadData();
-  res.render("allimages",{iamges: data})
+  res.render("allimages", { iamges: data })
 });
 
-router.post('/upload',upload, function(req,res,next){
-  console.log(req.file)
-  if(!req.file){
-    res.render("allimages", {error: "you need to upload a file"})
+router.post('/upload', upload, function (req, res, next) {
+  console.log("req file",req.file)
+  if (!req.file) {
+    res.render("allimages", { error: "you need to upload a file" })
   };
   const data = loadData();
-  data.push(req.file)
-  saveData(data);
-res.render("allimages", {images:data})
+  Jimp.read(req.file.path, (err, pic) => {
+    console.log("pic",pic)
+    if (err) throw err;
+    pic
+      .resize(256, 256) // resize
+      .quality(60) // set JPEG quality
+      .greyscale() // set greyscale
+      .write(req.file.path, ()=>{
+        data.push(req.file)
+        saveData(data);
+        res.render("allimages", { images: data })
+      }); // save
+    
+  });
+      
+  
+ 
 });
 
 module.exports = router;
