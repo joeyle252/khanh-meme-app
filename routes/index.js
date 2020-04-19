43576d4757deeb44848ea4b15bd328cc9411faf4
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-04-19 12:50:13 +0700

    feat: added text for images

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var jimp = require('jimp');
 var events = require('events');
-var em = events.EventEmitter();
 const path = require("path");
 const fs = require("fs")
 const upload = require("../utils/upload");
-const { loadData, saveData } = require("../utils/data.js");
+const { loadData, saveData, loadMemeData, saveMemeData } = require("../utils/data.js");
 
+const pathToMemepictures = path.join(__dirname, '../public/memepictures')
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: "Khanh's Meme app" });
@@ -34,10 +34,10 @@ router.post("/upload", upload, async (req, res) => {
   }
   try {
     let image = await jimp.read(file.path);
-    image.resize(300, jimp.AUTO, jimp.RESIZE_NEAREST_NEIGHBOR);
+    image.resize(300, 300, jimp.RESIZE_NEAREST_NEIGHBOR);
     await image.writeAsync(file.path);
 
-    image.id = data.length === 0 ? 1 : data[data.length - 1].id + 1;
+    file.id = data.length === 0 ? 1 : data[data.length - 1].id + 1;
     data.push(file);
     saveData(data);
 
@@ -46,12 +46,83 @@ router.post("/upload", upload, async (req, res) => {
     fs.unlinkSync(file.path);
     return res.render("index", { error: e.message })
   }
-})
+});
 
-router.get('/memes', async function (req,res){
-  const data = loadData();
-  res.render("allmemeimages", { images: data })
-})
+
+router.post("/addmeme", async (req, res) => {
+
+  const {top, bottom,id} = req.body;
+  if(!id ){
+    return res.render("error", {message: "please submit an ID"})
+  }
+  if (!top && !bottom){
+    return res.render('error',{message:"please submit some text"})
+  }
+  const data = loadData()
+  const selectedImageIndex = data.findIndex(image=>image.id === id*1)
+  if(selectedImageIndex === -1){
+    return res.render('allmemeimages', {message: "please submit some text"})
+  }
+  const selectedImage = data[selectedImageIndex]
+  let image = await jimp.read(selectedImage.path);
+  const font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+  image.print(
+    font,
+    0,
+    0,
+    {
+      text: top,
+      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: jimp.VERTICAL_ALIGN_TOP
+    },
+    image.bitmap.width,
+    image.bitmap.height,
+  );
+  image.print(
+    font,
+    0,
+    0,
+    {
+      text: bottom,
+      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: jimp.VERTICAL_ALIGN_BOTTOM
+    },
+    image.bitmap.width,
+    image.bitmap.height,
+  );
+let newName = Date.now().toString()+ selectedImage.filename
+  await image.writeAsync(`${pathToMemepictures}/${newName}`)
+
+  const memeData = loadMemeData();
+  let newmemeData = {
+    id: memeData.length > 0 ? memeData[memeData.length-1].id +1 : 1,
+    path: `${pathToMemepictures}/${newName}`,
+    filename: newName
+  }
+  memeData.push(newmemeData);
+  saveMemeData(memeData);
+  res.render('allmemeimages', {image: memeData})
+
+ });
+
+
+router.get("/meme", async (req, res) => {
+  
+
+});
+
+
+// 1. users click to pictures => show modal =>
+// 2. users fill text at input
+
+// 3. users click saved
+//  jimp get text from input => message and add to pictures
+
+// saveMemeData will push meme pictures to memes file
+
+
+
+
 
 
 module.exports = router;
